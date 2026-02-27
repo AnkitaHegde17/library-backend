ebffadc4b5e6c5d33186c02e1fd327995516625a
AnkitaHegde17 <ankitahegde6@gmail.com>
2026-02-28 00:51:51 +0530

    fixed mongodb atlas connection

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/books", require("./routes/books"));
-mongoose.connect(process.env.MONGO_URI)
-
+mongoose.connect("mongodb+srv://ankita_hegde:ankita_hegde@mywebapp.kvexlxx.mongodb.net/?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 app.use("/auth", require("./routes/auth"));
 
 app.listen(5000, () => console.log("Server running on 5000"));
